0c4062b29660f336ca79e3eb8d32f3b177a4e5b2
Tijana <tijanaboskovic973@gmail.com>
2026-02-27 00:50:26 +0100

    popravka za front za cloud 3

--- server/index.js
+++ server/index.js
@@ -29,7 +29,11 @@ const transporter = nodemailer.createTransport({
 const app = express();
 const lokalniKes = {};
 
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(helmet({ contentSecurityPolicy: false }));
 
