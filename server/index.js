a1c2a695ae5473d898459d3bc3f39d9a31699d56
Tijana <tijanaboskovic973@gmail.com>
2026-02-26 23:47:32 +0100

    port fix 3

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const cors = require("cors");
 const { startCron } = require("./eksterni/email");
@@ -12,6 +14,7 @@ const helmet = require('helmet');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const { dovuciDetalje } = require('./eksterni/detaljiOKnjizi');
+
 
 const nodemailer = require("nodemailer");
 const { Zaduzenje, Publikacija, Korisnik } = require("./models");
